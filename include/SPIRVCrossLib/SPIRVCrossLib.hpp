774b272260baef494a79fa90c655d248bf02b00b
Ray Cutler <ray@vidvox.net>
2025-06-17 09:19:13 -0400

    Adds necessary #includes

--- include/SPIRVCrossLib/SPIRVCrossLib.hpp
+++ include/SPIRVCrossLib/SPIRVCrossLib.hpp
@@ -1,6 +1,9 @@
 #ifndef SPIRVCROSSLIB_H
 #define SPIRVCROSSLIB_H
 
+#include <vector>
+#include <string>
+
 void SPIRVCrossLibFunc();
 
 bool ConvertVertSPIRVToMSL(const std::vector<uint32_t> & inSPIRVData, const std::string & inNewMainFuncName, std::string & outShaderString, std::string & outErrString);
